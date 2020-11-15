d88daab2f5edccc8967f883d9a91c0458430ce2e
araki777 <arakichi777@icloud.com>
2020-11-15 20:03:36 +0900

    タスク管理　ユーザーモデルの作成　ユーザー管理画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   root 'main#index'
   get :brain_training, to: 'brain_training#index'
-  get 'tasks#index', to: 'tasks#index'
+  resources :users
   resources :tasks
 end
