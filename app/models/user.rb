19d5ddbd1db70dec0ada299ce2625746e936bc4d
araki777 <arakichi777@example.com>
2020-11-30 00:07:31 +0900

    タスク管理画面　チャット機能の入力フォームのデザイン調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,6 @@ class User < ApplicationRecord
   has_many :groups, through: :group_users
   has_many :messages
 
-  enum role: { super_admin: 0, admin: 1, normal: 2, guest: 3 }
-
   def self.guest
     find_or_create_by!(name: 'guest', email: 'guest@example.com', bg_color_id: 1, role: 3) do |user|
       user.password = SecureRandom.urlsafe_base64
